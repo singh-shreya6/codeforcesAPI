b7b2912e3a59ec5601f4cab223275f77a18a7684
singh-shreya6 <singh.shreya8@gmail.com>
2020-07-04 19:14:11 +0530

    Refactoring, formatting and making Track B functional

--- codeforces-app/src/index.js
+++ codeforces-app/src/index.js
@@ -6,19 +6,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
-import  get  from 'lodash/get';
-import  Problems from './problems';
-import  { Link , BrowserRouter }  from 'react-router-dom';
+import get from 'lodash/get';
+import Problems from './problems';
+import { Link, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import maxresdefault from './maxresdefault.jpg';
 
 export class Home extends React.Component {
-  constructor(props) {
+	constructor(props) {
 		super(props);
-    this.state = {
+		this.state = {
 			problemSet: {},
 			isPageOpen: "0"
-    };
+		};
 	}
 
 	changePageSelected(a) {
@@ -30,72 +30,72 @@ export class Home extends React.Component {
 	}
 
 
-  render(){
+	render() {
 
 
-	  return (
+		return (
 			<div className="code_body">
-				 <Navbar bg="dark" variant="dark">
+				<Navbar bg="dark" variant="dark">
 					<Navbar.Brand href="/">Online Judge</Navbar.Brand>
 					<Nav activeKey={this.state.isPageOpen} onSelect={e => this.changePageSelected(e)}>
 						<Nav.Item>
 							<Nav.Link eventKey="0" href="#home">
 								Top 10
-        			</Nav.Link>	
+        			</Nav.Link>
 						</Nav.Item>
 						<Nav.Item>
 							<Nav.Link eventKey="1" href="#trackA">
 								Track A
-        			</Nav.Link>	
+        			</Nav.Link>
 						</Nav.Item>
 						<Nav.Item>
 							<Nav.Link eventKey="2" href="#trackB" >
 								Track B
-        			</Nav.Link>	
+        			</Nav.Link>
 						</Nav.Item>
 					</Nav>
-				</Navbar>	
-		
+				</Navbar>
 
-				<br/>
-				<br/>
+
+				<br />
+				<br />
 				<div className="card__body">
-				<Card style={{ width: '20%'}}>
-				<Card.Img variant="top" src={maxresdefault} />
-				<Card.Body>
-					<Card.Title>Binary Search</Card.Title>
-					<Card.Text>
-					 Binary Search is a search algorithm that finds the position of a target value within a sorted array.
+					<Card style={{ width: '20%' }}>
+						<Card.Img variant="top" src={maxresdefault} />
+						<Card.Body>
+							<Card.Title>Binary Search</Card.Title>
+							<Card.Text>
+								Binary Search is a search algorithm that finds the position of a target value within a sorted array.
 					</Card.Text>
-					<Button variant="primary">Start Solving!</Button>
-				</Card.Body>
-				</Card>
+							<Button variant="primary">Start Solving!</Button>
+						</Card.Body>
+					</Card>
 				</div>
-				<br/>
+				<br />
 				<Problems
-					problemSet = {this.state.problemSet}
-					isPageOpen = {this.state.isPageOpen}
+					problemSet={this.state.problemSet}
+					isPageOpen={this.state.isPageOpen}
 				/>
 			</div>
-    );
+		);
 	}
-	componentDidMount(){
+	componentDidMount() {
 		document.title = "Codeforces API";
 		fetch('https://codeforces.com/api/problemset.problems')
-		.then(results => {
+			.then(results => {
 				return results.json();
-		}).then(data => {
+			}).then(data => {
 				const problems = get(data, 'result.problems', {});
 				this.setState({
 					problemSet: problems
 				});
-		});
-		
-  }
+			});
+
+	}
 }
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+	<Home />,
+	document.getElementById('root')
 );
 
--- codeforces-app/src/problems.js
+++ codeforces-app/src/problems.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import  isEmpty  from 'lodash/isEmpty';
-import  filter  from 'lodash/filter';
-import  forEach  from 'lodash/forEach';
+import isEmpty from 'lodash/isEmpty';
+import filter from 'lodash/filter';
+import forEach from 'lodash/forEach';
 import Problem from './problem';
 
 class Problems extends React.Component {
@@ -11,15 +11,15 @@ class Problems extends React.Component {
         if (isEmpty(problems)) {
             return options;
         }
-        for (let i =0; i <10; i ++ ) {
+        for (let i = 0; i < 10; i++) {
             options.push(
-            <Problem 
-                problem = {problems[i]}
-            />)
+                <Problem
+                    problem={problems[i]}
+                />)
         }
         return options;
     }
-    createATable() {
+    createTableBasedOnIndex(problemIndex) {
         let options = [];
         let allAProblems = [];
         let problems = this.props.problemSet;
@@ -27,38 +27,47 @@ class Problems extends React.Component {
             return options;
         }
         allAProblems = filter(problems, problem => {
-            return problem.index === 'A';
+            return problem.index === problemIndex;
         });
         forEach(allAProblems, problem => {
             options.push(
-                <Problem 
-                    problem = {problem}
-            />);
+                <Problem
+                    problem={problem}
+                />);
         });
         return options;
     }
-    render(){
+    render() {
         if (this.props.isPageOpen === "0") {
-        return (
-            <div>
-                <h2>PROBLEMSET</h2>
-				<h3>10 Latest Problems</h3>
-				<br/>
-                  {this.createProblemTable()}
-                <br/>
-            </div>
-                
-      );
-        } else {
+            return (
+                <div>
+                    <h2>PROBLEMSET</h2>
+                    <h3>10 Latest Problems</h3>
+                    <br />
+                    {this.createProblemTable()}
+                    <br />
+                </div>
+            );
+        } else if (this.props.isPageOpen === "1") {
             return (
                 <div className="a_problems">
                     <h2>PROBLEMSET</h2>
-                    <br/>
+                    <br />
                     <h3>All A problems</h3>
-                    <br/>
-                      {this.createATable()}
-                  </div>
-          );
+                    <br />
+                    {this.createTableBasedOnIndex('A')}
+                </div>
+            );
+        } else {
+            return (
+                <div className="b_problems">
+                    <h2>PROBLEMSET</h2>
+                    <br />
+                    <h3>All B problems</h3>
+                    <br />
+                    {this.createTableBasedOnIndex('B')}
+                </div>
+            );
         }
     }
 }
