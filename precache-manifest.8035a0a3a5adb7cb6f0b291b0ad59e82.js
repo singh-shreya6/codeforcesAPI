3230d180807aa22730d091e57824fe6714eeefc3
singh-shreya6 <singh.shreya8@gmail.com>
2020-07-05 14:53:08 +0530

    Updates

---
+++ precache-manifest.8035a0a3a5adb7cb6f0b291b0ad59e82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32dba96915ca1dd172060b75c7273544",
+    "url": "/static/media/maxresdefault.32dba969.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad2d776ce44ab847b2f8",
+    "url": "/static/js/main.ad2d776c.chunk.js"
+  },
+  {
+    "revision": "27c441457a07e080b23f",
+    "url": "/static/js/1.27c44145.chunk.js"
+  },
+  {
+    "revision": "ad2d776ce44ab847b2f8",
+    "url": "/static/css/main.d5db6f11.chunk.css"
+  },
+  {
+    "revision": "27c441457a07e080b23f",
+    "url": "/static/css/1.424a771b.chunk.css"
+  },
+  {
+    "revision": "3205b317abe08ff752d2c238cfb825ac",
+    "url": "/index.html"
+  }
+];
