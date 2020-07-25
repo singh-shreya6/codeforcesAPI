ecf14372380201c12bb5280f5436f38c40750127
singh-shreya6 <singh.shreya8@gmail.com>
2020-07-25 16:38:44 +0530

    Latest search page changes

--- src/searchPage.js
+++ src/searchPage.js
@@ -4,22 +4,38 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import get from 'lodash/get';
 
+
 class SearchPage extends React.Component {
     constructor(props) {
         super(props);
         this.textInput = React.createRef();
+        this.state = {
+            profile: {}
+        };
     }
 
     fetchUserInfo() {
         const value = this.textInput.current.value
-        fetch('https://codeforces.com/api/user.info?handles='+value)
+        fetch('https://codeforces.com/api/user.info?handles=' + value)
             .then(results => {
                 return results.json();
             }).then(data => {
-				const user = get(data, 'result', {});
-                console.log(user);
-			});
+                const user = get(data, 'result', {});
+                this.state.profile = ({
+                    profile: user
+                });
+            });
+        this.displayProfile();
     }
+
+    displayProfile() {
+        let user = this.state.profile;
+        console.log(user);
+        return (
+            <p>Hello {user[0].handle}</p>
+        );
+    }
+
     render() {
         if (this.props.isPageOpen === "6") {
             return (
