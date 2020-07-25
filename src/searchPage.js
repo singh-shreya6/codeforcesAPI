c4121dcf0cc312097a482af05e506e0742e17c07
singh-shreya6 <singh.shreya8@gmail.com>
2020-07-25 18:53:31 +0530

    Add profile in Search Page

--- src/searchPage.js
+++ src/searchPage.js
@@ -3,14 +3,15 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import get from 'lodash/get';
-
+import Card from 'react-bootstrap/Card';
 
 class SearchPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             profile: {},
-            handle: ''        
+            handle: '',
+            clicked: false
         };
     }
 
@@ -22,16 +23,51 @@ class SearchPage extends React.Component {
             }).then(data => {
                 const user = get(data, 'result', {});
                 this.setState({
-                    profile: user[0]
+                    profile: user[0],
+                    clicked: true
                 });
             });
     }
 
+    getName(user) {
+        if (user.firstName !== undefined && user.lastName !== undefined) {
+            return user.firstName + '' + user.lastName;
+        } else if (user.firstName !== undefined) {
+            return user.firstName;
+        } else {
+            return '';
+        }
+    }
+
     displayProfile() {
         let user = this.state.profile;
-        return (
-            <p>Hello {user.handle}</p>
-        );
+        if (this.state.clicked && user !== undefined) {
+            return (
+                <div className="card__body">
+                    <Card style={{ width: '20%', margin: '0 2rem 0 0' }}>
+                        <Card.Img variant="top" src={user.titlePhoto} className="card" />
+                        <Card.Body>
+                            <div className="card_text">
+                                <Card.Title>{user.handle} <br /> {this.getName(user)}</Card.Title>
+                                <Card.Text>
+                                    Rank: {user.rank.charAt(0).toUpperCase() +
+                                        user.rank.slice(1)}<br />
+                                Rating: {user.rating}<br />
+                                Country: {user.country}<br />
+                                Maximum Rank: {user.maxRank.charAt(0).toUpperCase() +
+                                        user.maxRank.slice(1)}<br />
+                                Friend of: {user.friendOfCount} users<br />
+                                Organisation: {user.organization}<br />
+
+                                </Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
+            );
+        } else {
+            return <div></div>;
+        }
     }
 
     handleOnTextChange(e) {
@@ -43,7 +79,7 @@ class SearchPage extends React.Component {
     render() {
         if (this.props.isPageOpen === "6") {
             return (
-                <div className="handle" >
+                <div className="search_bar" >
                     <label> Enter Codeforces Handle </label>
                     <InputGroup className="mb-3">
                         <FormControl
